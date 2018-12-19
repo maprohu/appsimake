d3ce1e8d5cf358bf1f14a826f51e45ae60e94bfb
maprohu <maprohu@gmail.com>
2018-12-19 15:11:21 +0000

    deploy

--- builder/buildserver/src/buildserver/triggerdeploy.kt
+++ builder/buildserver/src/buildserver/triggerdeploy.kt
@@ -5,5 +5,6 @@ import java.net.URL
 fun main(args: Array<String>) {
     "git add -A .".runCommand()
     "git commit -m deploy".runCommand()
+    "git push".runCommand()
     println(URL("https://pullanapp-maprohu.c9users.io/build").readText())
 }
